5a76b0c7098d60409dacbea5aba05153bc91eb14
brandon <github@thequinn.org>
2023-04-23 14:38:38 -0500

    Completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let total = 0
 
+    if (((numOne < 0 || numTwo < 0)) || (!Number.isInteger(numOne) || !Number.isInteger(numTwo)))
+    {
+        return "ERROR";
+    }
+        
+    else if (numOne > numTwo){
+        
+        for(i= numTwo; i <= numOne; i++){
+            total = total + i;
+        }
+    }
+
+    else {
+        for(i= numOne; i <= numTwo; i++){
+            total = total + i;
+        }
+    }
+    
+    return total;
 };
 
 // Do not edit below this line
