4629e1f376b18c873a0608a8f61bedc9437b114c
brandon <github@thequinn.org>
2023-05-15 12:04:19 -0500

    completed calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let total = 0
+  for (i=0; i < numbers.length; i++){
+    total = total + numbers[i]
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let total = 1
+  for (i=0; i < numbers.length; i++){
+    total = total * numbers[i]
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number,exponent) {
+  return number**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 1 || number === 0){
+    return 1;
+  }
+  for (i=number - 1; i > 0; i--){
+    number *= i
+  }
+  return number;
 };
 
 // Do not edit below this line
