38bf59e2b6e50e43edefaa7b9bc4aca03321f95b
brandon <github@thequinn.org>
2023-05-16 10:01:39 -0500

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0){
+        return "OOPS";
+    }
+    let numbers = [1, 1];
+    for (i = 1; i < num - 1; i++){
+        let previousNum = numbers[i] + numbers[i - 1];
+        numbers.push(previousNum);
+    }
+    return numbers[num - 1];
 };
 
 // Do not edit below this line
